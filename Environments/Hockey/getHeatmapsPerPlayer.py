5c4ad78c0edce03a6d7d0cefa8a08e2c10f96709
Eric-H-Thomas <103595220+Eric-H-Thomas@users.noreply.github.com>
2025-11-22 08:37:38 -0700

    Clarify hockey heatmap splitter

--- Environments/Hockey/getHeatmapsPerPlayer.py
+++ Environments/Hockey/getHeatmapsPerPlayer.py
@@ -1,83 +1,116 @@
+"""Utility for splitting hockey heatmap data by player and shot type.
+
+The script expects to be executed from the root of the repository and the
+experiment folder name to be passed as the sole command line argument.  The
+folder structure is assumed to follow the hierarchy created by the experiment
+pipelines (Experiments/hockey-multi/<experiment>/Data/).
+"""
+
+import os
 import pickle
-import code
-import os,sys
+import sys
 
 
-if __name__ == '__main__':
-	
-
-	try:
-		experimentFolder = sys.argv[1]
-	except Exception as e:
-		print("Need to specify the name of the folder for the experiment (located under 'Experiments/hockey-multi/') as command line argument. (Make sure the data (.pkl) file is located inside such folder too.)")
-		exit()
-
-
-
-	folder = f"Experiments{os.sep}hockey-multi{os.sep}{experimentFolder}{os.sep}Data{os.sep}"
-	fileName = f"heatmap_data.pkl"
-
-	try:
-		with open(folder+fileName,"rb") as infile:
-			data = pickle.load(infile)
-	except Exception as e:
-		print(e)
-		print("File with data not present. Please place the data (.pkl) file inside the folder for the experiment.")
-		exit()
+def _load_data_file(folder: str, filename: str):
+    """Load the aggregated heatmap pickle file."""
+    # The master pickle file contains shots for all players.
+    # Loading it upfront allows us to later split it by player/shot type.
+    try:
+        with open(os.path.join(folder, filename), "rb") as infile:
+            return pickle.load(infile)
+    except Exception as exc:  # pragma: no cover - passthrough error reporting
+        print(exc)
+        print(
+            "File with data not present. Please place the data (.pkl) file inside the folder for the experiment."
+        )
+        sys.exit()
 
 
-	if not os.path.exists(f"{folder}Heatmaps{os.sep}"):
-		os.mkdir(f"{folder}Heatmaps{os.sep}")
+def _ensure_heatmap_dir(folder: str) -> str:
+    """Create the Heatmaps/ directory inside the experiment folder if absent."""
+
+    # Files for each player are emitted to Experiments/.../Data/Heatmaps/.
+    # Centralizing the directory creation avoids repetition.
+    heatmap_dir = os.path.join(folder, "Heatmaps")
+    if not os.path.exists(heatmap_dir):
+        os.mkdir(heatmap_dir)
+    return heatmap_dir
 
 
-	playerData = {}
+def _group_shots_by_player(data):
+    """Group rows by shooter and shot type."""
 
-	typeShots = []
+    player_data = {}
+    shot_types = []
 
+    for row in data:
+        # Each row is keyed by a unique ID; its contents specify the player and
+        # the type of shot they took.
+        pid = data[row]["shooter_id"]
+        type_shot = data[row]["shot_type"]
 
-	# Group data by player ID
-	for row in data:
+        if pid not in player_data:
+            player_data[pid] = {}
+        if type_shot not in player_data[pid]:
+            player_data[pid][type_shot] = {}
 
-		pid = data[row]["shooter_id"]
+        player_data[pid][type_shot][row] = data[row]
 
-		if pid not in playerData:
-			playerData[pid] = {}
+        if type_shot not in shot_types:
+            shot_types.append(type_shot)
 
-
-		typeShot = data[row]["shot_type"]
-
-		if typeShot not in typeShots:
-			typeShots.append(typeShot)
-
-		if typeShot not in playerData[pid]:
-			playerData[pid][typeShot] = {}
-
-
-		if row not in playerData[pid][typeShot]:
-			playerData[pid][typeShot][row] = {}
-
-		playerData[pid][typeShot][row] = data[row]
+    return player_data, shot_types
 
 
-	# Create new pickle file per player ID
-	for eachID in playerData:		
-		for typeShot in playerData[eachID]:
-			with open(f"{folder}Heatmaps{os.sep}heatmap_data_player_{eachID}_type_shot_{typeShot}.pkl","wb") as outfile:
-				pickle.dump(playerData[eachID][typeShot],outfile)
+def _write_player_pickles(folder: str, player_data: dict):
+    """Write one pickle file per (player, shot type) combination."""
+
+    heatmap_dir = _ensure_heatmap_dir(folder)
+    for player_id, shots in player_data.items():
+        for shot_type, entries in shots.items():
+            # File names encode both identifiers so downstream scripts can
+            # locate the specific dataset they require.
+            file_name = f"heatmap_data_player_{player_id}_type_shot_{shot_type}.pkl"
+            with open(os.path.join(heatmap_dir, file_name), "wb") as outfile:
+                pickle.dump(entries, outfile)
 
 
-	with open(f"{folder}stats-AllData.txt","w") as outfile:
-		print(f"Total number of players: {len(playerData)}",file=outfile)
-		print(file=outfile)
+def _write_summary_stats(folder: str, player_data: dict, shot_types: list):
+    """Generate a text file with counts per player and shot type."""
 
-		for eachID in playerData:
-			print(f"Player: {eachID}",file=outfile)
+    stats_path = os.path.join(folder, "stats-AllData.txt")
+    with open(stats_path, "w") as outfile:
+        print(f"Total number of players: {len(player_data)}", file=outfile)
+        print(file=outfile)
 
-			for st in typeShots:
-				if st in playerData[eachID]:
-					print(f"\t{st}: {len(playerData[eachID][st])}",file=outfile)
+        for player_id in player_data:
+            print(f"Player: {player_id}", file=outfile)
+            for shot_type in shot_types:
+                # Only print counts for shot types recorded for the player.
+                if shot_type in player_data[player_id]:
+                    count = len(player_data[player_id][shot_type])
+                    print(f"\t{shot_type}: {count}", file=outfile)
 
 
-	# code.interact("...", local=dict(globals(), **locals()))
+def main():
+    """Entry point when executing the module as a script."""
+
+    try:
+        experiment_folder = sys.argv[1]
+    except Exception:  # pragma: no cover - command line validation only
+        print(
+            "Need to specify the name of the folder for the experiment (located under 'Experiments/hockey-multi/') as command line argument. (Make sure the data (.pkl) file is located inside such folder too.)"
+        )
+        sys.exit()
+
+    folder = os.path.join("Experiments", "hockey-multi", experiment_folder, "Data")
+    file_name = "heatmap_data.pkl"
+
+    data = _load_data_file(folder, file_name)
+    player_data, shot_types = _group_shots_by_player(data)
+    _write_player_pickles(folder, player_data)
+    _write_summary_stats(folder, player_data, shot_types)
 
 
+if __name__ == "__main__":
+    main()
